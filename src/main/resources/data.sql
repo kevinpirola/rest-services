837bee8e290310e06f6e8560b91c4e14197f5997
kevinpirola <gianmarco.laggia@gmail.com>
2016-06-15 00:27:36 +0200

    fixed bug

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 DELETE FROM UTENTE;
 
-INSERT INTO UTENTE VALUES (1, 'admin', 'admin', 'postmaster@larus-ba.it', 'S');
-INSERT INTO UTENTE VALUES (2, 'user', 'user', 'postmaster@larus-ba.it', 'S');
+INSERT INTO UTENTE VALUES (1, 'admin', 'admin', 'postmaster@larus-ba.it');
+INSERT INTO UTENTE VALUES (2, 'user', 'user', 'postmaster@larus-ba.it');
